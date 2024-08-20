e10f31c26a4c92f3c76a625c9ec386ec280985f0
frest1ler <143139569+frest1ler@users.noreply.github.com>
2024-08-20 18:45:45 +0300

    initial commit

---
+++ Square_Solver.cpp
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <assert.h>
+const int INFINITY_ROOTS = -1;
+const int NO_ROOTS = 0;
+int Data_Entry(double* a, double* b, double* c);
+int Square_Solver(double a, double b, double c, double x1, double x2);
+int main()
+{
+    double a = 0, b = 0, c = 0, x1 = 0, x2 = 0;
+    Data_Entry( &a, &b, &c );
+    Square_Solver( a, b, c, x1, x2);
+}
+int Data_Entry(double* a, double* b, double* c)
+{
+    printf("a=");
+    scanf("%lg",a);
+    printf("\n");
+    printf("b=");
+    scanf("%lg",b);
+    printf("\n");
+    printf("c=");
+    scanf("%lg",c);
+    printf("\n"); //input a,b,c
+    assert(isfinite(*a));
+    assert(isfinite(*b));
+    assert(isfinite(*c));
+}
+int Square_Solver(double a, double b, double c, double x1, double x2)
+{
+    int nRoots;
+    double d;
+    double sqrt_d;
+    if ( a == 0 )
+    {
+        if ( b == 0)
+        {
+            if ( c == 0)
+                nRoots = INFINITY_ROOTS;
+            else
+                nRoots = NO_ROOTS;
+        }
+        else // ( b != 0 )
+        {
+            nRoots = 1;
+            x1 = x2 = -c / b;
+        }
+    }
+    else // ( a != 0)
+    {
+        d = b * b - 4 * a * c;
+        if ( d < 0 )
+        nRoots = NO_ROOTS;
+        if ( d == 0 )
+        {
+            nRoots = 1;
+            x1 = x2 = -b / (2 * a);
+        }
+        if ( d > 0 )
+        {
+            nRoots = 2;
+            sqrt_d = sqrt ( d );
+            x1 = ( -b + sqrt_d ) / ( 2 * a );
+            x2 = ( -b - sqrt_d ) / ( 2 * a );
+        }
+    }
+    switch ( nRoots )
+    {
+        case NO_ROOTS :
+        {
+            printf("Nule roots");
+            break;
+        }
+        case 1 :
+        {
+            printf("One roots x=%lg", x1);
+            break;
+        }
+        case 2 :
+        {
+            printf("Two roots x1=%lg , x2=%lg", x1, x2);
+            break;
+        }
+        case INFINITY_ROOTS :
+        {
+            printf("infinitely many roots");
+            break;
+        }
+        default :
+        {
+            printf("ERROR");
+            break;
+        }
+    }
+}
