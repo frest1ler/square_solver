2f788ff6cc1a82ae1282babc3071376401e2e607
frest1ler <143139569+frest1ler@users.noreply.github.com>
2024-08-21 14:24:30 +0300

    added assert
    added constants for switch, added assert, fixed checking with epsilon neighborhood

--- square_solver.cpp
+++ square_solver.cpp
@@ -5,6 +5,8 @@
 
 const int INFINITY_ROOTS = -1;
 const int NO_ROOTS = 0;
+const int ONE_ROOTS = 1;
+const int TWO_ROOTS = 2;
 const double EPSILON = 1e-9;
 
 int data_entry(double* a, double* b, double* c);
@@ -15,7 +17,7 @@ int compare_with_zero (double x);
 
 int main()
 {
-    double a = 0, b = 0, c = 0, x1 = 0, x2 = 0;
+    double a = NAN, b = NAN, c = NAN, x1 = NAN, x2 = NAN;
     int number_roots = 0;
 
     data_entry(&a, &b, &c);
@@ -46,38 +48,39 @@ int data_entry(double* a, double* b, double* c)
     scanf("%lg",c);
 
     printf("\n"); //input a,b,c
-
+    assert(std::isfinite(*a));
+    assert(std::isfinite(*b));
+    assert(std::isfinite(*c));
 }
 
 int square_solver(double a, double b, double c, double* x1, double* x2, int* number_roots)
 {
     if (compare_with_zero(b) == 0 && compare_with_zero(c) == 0)
     {
-        *x1 = *x2 = fabs(-b / (2 * a));
-        *number_roots = 1;
+        *x1 = *x2 = 0;
+        *number_roots = ONE_ROOTS;
     }
     else
     { // compare_with_zero(b) != 0 || compare_with_zero(c) != 0
-        double d = NAN, sqrt_d = NAN;
+        double discr = NAN;
 
-        d = b * b - 4 * a * c;
+        discr = b * b - 4 * a * c;
 
-        if (d < 0)
+        if (discr < 0)
         {
             *number_roots = NO_ROOTS;
         }
-        if (d == 0)
+        if (discr == 0)
         {
-            *number_roots = 1;
+            *number_roots = ONE_ROOTS;
             *x1 = *x2 = -b / (2 * a);
         }
 
-        if (d > 0)
+        if (discr > 0)
         {
-        *number_roots = 2;
-        sqrt_d = sqrt(d);
-        *x1 = (-b + sqrt_d) / (2 * a);
-        *x2 = (-b - sqrt_d) / (2 * a);
+            *number_roots = TWO_ROOTS;
+            *x1 = (-b + sqrt(discr)) / (2 * a);
+            *x2 = (-b - sqrt(discr)) / (2 * a);
         }
     }
 }
@@ -91,12 +94,12 @@ int data_output(int number_roots, double x1, double x2)
             printf("Nule roots\n");
             break;
         }
-        case 1 :
+        case ONE_ROOTS :
         {
             printf("One roots x=%lg\n", x1);
             break;
         }
-        case 2 :
+        case TWO_ROOTS :
         {
             printf("Two roots x1=%lg , x2=%lg\n", x1, x2);
             break;
@@ -132,12 +135,12 @@ int linear_solver(double b, double c, double* x1, double* x2, int* number_roots)
     {
         if (compare_with_zero(c) == 0)
         {
-            *x1 = *x2 = fabs(-c / b);
-            *number_roots = 1;
+            *x1 = *x2 = 0;
+            *number_roots = ONE_ROOTS;
         }
         else
         {
-            *number_roots = 1;
+            *number_roots = ONE_ROOTS;
             *x1 = *x2 = -c / b;
         }
     }
@@ -145,7 +148,7 @@ int linear_solver(double b, double c, double* x1, double* x2, int* number_roots)
 
 int compare_with_zero (double x)    // comparing coeffs with zero
 {
-    if (fabs(x) < EPSILON) return 0;
-    else if (x < -EPSILON) return -1;
+    if (fabs(x) <  EPSILON) return 0;
+    else if (x  < -EPSILON) return -1;
     else return 1;
 }
