7d55b27c6b9e287e3cea4303a0b737ba6f23f3c8
frest1ler <143139569+frest1ler@users.noreply.github.com>
2024-08-30 00:58:25 +0300

    refactoring
    refactoring

--- square_solver.cpp
+++ square_solver.cpp
@@ -3,35 +3,40 @@
 #include <stdlib.h>
 #include <assert.h>
 
-enum const_data
+enum number_roots
 {
-    INFINITY_ROOTS = -1,
-    NO_ROOTS       =  0,
-    ONE_ROOTS      =  1,
-    TWO_ROOTS      =  2,
-    LESS_THAN_EPSILON               = -1,
-    INSIDE_THE_EPSILON_NEIGHBORHOOD = 0,
-    MORE_EPSILON                    = 1,
+    INFINITY_ROOTS                  = -1,
+    NO_ROOTS                        =  0,
+    ONE_ROOTS                       =  1,
+    TWO_ROOTS                       =  2
 };
 
-const double EPSILON         =  1e-9;
-void input_data(struct equalization_coefficients *input);
+enum compare_constants
+{
+    LESS_THAN_EPSILON               = -1,
+    INSIDE_THE_EPSILON_NEIGHBORHOOD =  0,
+    MORE_EPSILON                    =  1
+};
+
+const double EPSILON = 1e-9;
+
+void input_data(struct equation_coefficients *coefficients);
 void clear_buffer();
-void math_calculus(struct equalization_coefficients *input, struct roots_of_equation *output);
-void square_solver(struct equalization_coefficients *input, struct roots_of_equation *output);
-void linear_solver(struct equalization_coefficients *input, struct roots_of_equation *output);
-void output_data(struct roots_of_equation *output);
-void test_one(struct test_data *data_for_test, struct roots_of_equation *output, int i);
-void test(struct test_data *data_for_test, struct roots_of_equation *output, const int MAX_NUMBER_TEST);
+void math_calculus(struct equation_coefficients *coefficients, struct equation_roots *roots);
+void square_solver(struct equation_coefficients *coefficients, struct equation_roots *roots);
+void linear_solver(struct equation_coefficients *coefficients, struct equation_roots *roots);
+void output_data(struct equation_roots *roots);
+void test_one(struct test_data *data_for_test, struct equation_roots *roots, int i);
+void test();
 int  compare_with_zero (double x);
 
-struct equalization_coefficients //TODO RENAME
+struct equation_coefficients
 {
     double a;
     double b;
     double c;
 };
-struct roots_of_equation
+struct equation_roots
 {
     double x1;
     double x2;
@@ -40,97 +45,82 @@ struct roots_of_equation
 
 struct test_data
 {
-    struct equalization_coefficients input_test;
-    struct roots_of_equation         output_test;
-    int                              number_test;
+    struct equation_coefficients coefficients;
+    struct equation_roots        roots;
+    int                          number_test;
 };
 
 int main()
 {
-    struct test_data data_for_test[] =
-    {
-        {{1, -5, 6}, {2, 3, 2}, 0},
-        {{0, 0, 0}, {0, 0, -1}, 1}
-    };
-    struct equalization_coefficients input = {NAN, NAN, NAN};
-    struct roots_of_equation         output = {NAN, NAN, 0};
+    struct equation_coefficients coefficients  = {NAN, NAN, NAN};
+    struct equation_roots        roots         = {NAN, NAN, 0};
 
-    const int MAX_NUMBER_TEST = sizeof(data_for_test) / sizeof(test_data);
+    test();
 
-    input_data(&input);
+    input_data(&coefficients);
 
-    math_calculus(&input, &output);
+    math_calculus(&coefficients, &roots);   //TODO mask_anus
 
-    output_data(&output);
-
-    test(data_for_test, &output, MAX_NUMBER_TEST);
+    output_data(&roots);
 }
 
-void input_data(struct equalization_coefficients *input)
+void input_data(struct equation_coefficients *coefficients)
 {
     printf("Hi,hi, this program solves an equation of the form ax^2+bx + c = 0\n"
            "enter a, b, c\n");
-    while((scanf("%lg%lg%lg", &(input->a), &(input->b), &(input->c))) != 3
-          || (isfinite(input->a)) == 0 || (isfinite(input->b)) == 0 || (isfinite(input->c)) == 0)
+
+    while((scanf("%lg %lg %lg", &(coefficients->a), &(coefficients->b), &(coefficients->c))) != 3)
     {
         clear_buffer();
         printf("Input error. Try again\a\n");
     }
     clear_buffer();
-
-    assert(isfinite(input->a));
-    assert(isfinite(input->b));
-    assert(isfinite(input->c));
-
-    assert(&input->a != &input->b);
-    assert(&input->a != &input->c);
-    assert(&input->b != &input->c);
 }
 
-void square_solver(struct equalization_coefficients *input, struct roots_of_equation *output)
+void square_solver(struct equation_coefficients *coefficients, struct equation_roots *roots)
 {
-    assert(isfinite(input->a));
-    assert(isfinite(input->b));
-    assert(isfinite(input->c));
+    assert(isfinite(coefficients->a));//бро красава
+    assert(isfinite(coefficients->b));
+    assert(isfinite(coefficients->c));
 
-    assert(&output->x1); //TODO adress struct != 0
-    assert(&output->x2);
-    if (compare_with_zero(input->b) == INSIDE_THE_EPSILON_NEIGHBORHOOD &&
-        compare_with_zero(input->c) == INSIDE_THE_EPSILON_NEIGHBORHOOD)
+    assert(roots);//bro cool
+
+    if (compare_with_zero(coefficients->b) == INSIDE_THE_EPSILON_NEIGHBORHOOD &&
+        compare_with_zero(coefficients->c) == INSIDE_THE_EPSILON_NEIGHBORHOOD)
     {
-        output->x1 = output->x2 = 0;
-        output->number_roots = ONE_ROOTS;
+        roots->x1 = roots->x2 = 0;
+        roots->number_roots = ONE_ROOTS;
     }
     else
     { // compare_with_zero(b) != 0 || compare_with_zero(c) != 0
         double discriminant = NAN, sqrt_discriminant = NAN;
 
-        discriminant = input->b * input->b - 4 * input->a * input->c;
+        discriminant = coefficients->b * coefficients->b - 4 * coefficients->a * coefficients->c;
 
         if (compare_with_zero(discriminant) == LESS_THAN_EPSILON)
         {
-            output->number_roots = NO_ROOTS;
-            output->x1 = output->x2 = 0;
+            roots->number_roots = NO_ROOTS;
+            roots->x1 = roots->x2 = 0;
         }
         if (compare_with_zero(discriminant) == INSIDE_THE_EPSILON_NEIGHBORHOOD)
         {
-            output->number_roots = ONE_ROOTS;
-            output->x1 = output->x2 = -input->b / (2 * input->a);
+            roots->number_roots = ONE_ROOTS;
+            roots->x1 = roots->x2 = -coefficients->b / (2 * coefficients->a);
         }
 
         if (compare_with_zero(discriminant) == MORE_EPSILON)
         {
-            output->number_roots = TWO_ROOTS;
+            roots->number_roots = TWO_ROOTS;
             sqrt_discriminant = sqrt(discriminant);
-            output->x1 = (-input->b + sqrt_discriminant) / (2 * input->a);
-            output->x2 = (-input->b - sqrt_discriminant) / (2 * input->a);
+            roots->x1 = (-coefficients->b + sqrt_discriminant) / (2 * coefficients->a);
+            roots->x2 = (-coefficients->b - sqrt_discriminant) / (2 * coefficients->a);
         }
     }
 }
 
-void output_data(struct roots_of_equation *output)
+void output_data(struct equation_roots *roots)
 {
-    switch (output->number_roots)
+    switch (roots->number_roots)
     {
         case NO_ROOTS :
         {
@@ -139,12 +129,12 @@ void output_data(struct roots_of_equation *output)
         }
         case ONE_ROOTS :
         {
-            printf("One roots x=%lg\n", output->x1);
+            printf("One roots x=%lg\n", roots->x1);
             break;
         }
         case TWO_ROOTS :
         {
-            printf("Two roots x1=%lg , x2=%lg\n", output->x1, output->x2);
+            printf("Two roots x1=%lg , x2=%lg\n", roots->x1, roots->x2);
             break;
         }
         case INFINITY_ROOTS :
@@ -160,38 +150,37 @@ void output_data(struct roots_of_equation *output)
     }
 }
 
-void linear_solver(struct equalization_coefficients *input, struct roots_of_equation *output)
+void linear_solver(struct equation_coefficients *coefficients, struct equation_roots *roots)
 {
-    assert(isfinite(input->a));
-    assert(isfinite(input->b));
-    assert(isfinite(input->c));
+    assert(isfinite(coefficients->a));
+    assert(isfinite(coefficients->b));
+    assert(isfinite(coefficients->c));
 
-    assert(&output->x1);
-    assert(&output->x2);
+    assert(roots);
 
-    if (compare_with_zero(input->b) == INSIDE_THE_EPSILON_NEIGHBORHOOD)
+    if (compare_with_zero(coefficients->b) == INSIDE_THE_EPSILON_NEIGHBORHOOD)
     {
-        if (compare_with_zero(input->c) == INSIDE_THE_EPSILON_NEIGHBORHOOD)
+        if (compare_with_zero(coefficients->c) == INSIDE_THE_EPSILON_NEIGHBORHOOD)
         {
-            output->number_roots = INFINITY_ROOTS;
-            output->x1 = output->x2 = 0;
+            roots->number_roots = INFINITY_ROOTS;
+            roots->x1 = roots->x2 = 0;
         }
         else
         {
-            output->number_roots = NO_ROOTS;
+            roots->number_roots = NO_ROOTS;
         }
     }
     else
     {
-        if (compare_with_zero(input->c) == INSIDE_THE_EPSILON_NEIGHBORHOOD)
+        if (compare_with_zero(coefficients->c) == INSIDE_THE_EPSILON_NEIGHBORHOOD)
         {
-            output->x1 = output->x2 = 0;
-            output->number_roots = ONE_ROOTS;
+            roots->x1 = roots->x2 = 0;
+            roots->number_roots = ONE_ROOTS;
         }
         else
         {
-            output->number_roots = ONE_ROOTS;
-            output->x1 = output->x2 = -input->c / input->b;
+            roots->number_roots = ONE_ROOTS;
+            roots->x1 = roots->x2 = -coefficients->c / coefficients->b;
         }
     }
 }
@@ -209,15 +198,15 @@ int compare_with_zero(double x)    // comparing coeffs with zero
     else return MORE_EPSILON;
 }
 
-void math_calculus(struct equalization_coefficients *input, struct roots_of_equation *output) //TODO rename
+void math_calculus(struct equation_coefficients *coefficients, struct equation_roots *roots)
 {
-   if (compare_with_zero(input->a) != INSIDE_THE_EPSILON_NEIGHBORHOOD)
+   if (compare_with_zero(coefficients->a) != INSIDE_THE_EPSILON_NEIGHBORHOOD)
     {
-        square_solver(input, output);
+        square_solver(coefficients, roots);
     }
     else
     {
-        linear_solver(input, output);
+        linear_solver(coefficients, roots);
     }
 }
 void clear_buffer()
@@ -230,33 +219,44 @@ void clear_buffer()
 	} while (clean != '\n' && clean != EOF);
 }
 
-void test_one(struct test_data *data_for_test, struct roots_of_equation *output, int i)
+void test_one(struct test_data *data_for_test, struct equation_roots *roots, int i) //ПЕИМЕНОВАТЬ ХУЙНЮ
 {
-    output->x1 = NAN;
-    output->x2 = NAN;
-    output->number_roots = 0;
+    roots->x1 = NAN;
+    roots->x2 = NAN;
+    roots->number_roots = 0;
 
-    math_calculus(&data_for_test[i].input_test, output);
-    if (compare_with_zero(output->x1 - data_for_test[i].output_test.x1) != 0 &&
-    compare_with_zero(output->x2 - data_for_test[i].output_test.x2) != 0 &&
-        output->number_roots != data_for_test[i].output_test.number_roots)
+    math_calculus(&data_for_test[i].coefficients, roots);//TODO ЧУТЬ ЧУТЬ ХУЙНЮ ПРЕКРАТИТЬ, ПРОБЕЛ
+    if (compare_with_zero(roots->x1 - data_for_test[i].roots.x1) != 0 &&
+    compare_with_zero(roots->x2 - data_for_test[i].roots.x2) != 0 &&
+        roots->number_roots != data_for_test[i].roots.number_roots)
     {
         printf("\n%d TEST ERROR , got the results:\n\nx1=%lg ; expected x1=%lg\n"
                "x2=%lg ; expected x2=%lg\n"
                "number_roots=%d ; expected number_roots=%d\n\n",
-               data_for_test[i].number_test, output->x1, data_for_test[i].output_test.x1,
-               output->x2, data_for_test[i].output_test.x2,
-               output->number_roots, data_for_test[i].output_test.number_roots);
+               data_for_test[i].number_test, roots->x1, data_for_test[i].roots.x1,
+               roots->x2, data_for_test[i].roots.x2,
+               roots->number_roots, data_for_test[i].roots.number_roots);
     }
     else
     {
         printf("TEST OK\n");
     }
 }
-void test(struct test_data *data_for_test, struct roots_of_equation *output, const int MAX_NUMBER_TEST)
+void test()
 {
-    for(int i = 0; i < MAX_NUMBER_TEST; i++)
+    struct test_data data_for_test[] =
     {
-        test_one(&data_for_test[i], output, i);
+        {{1, -5, 6}, {2, 3, 2}, 0},//TODO ДОБАВИТЬ ХУЙНЮ(СОРТИРОВКУ)
+        {{0,  0, 0}, {0, 0, -1}, 1}
+    };
+
+    struct equation_coefficients coefficients  = {NAN, NAN, NAN};
+    struct equation_roots        roots         = {NAN, NAN, 0};
+
+    const int max_number_test = sizeof(data_for_test) / sizeof(data_for_test[0]);
+
+    for(int i = 0; i < max_number_test; i++)
+    {
+        test_one(data_for_test + i, &roots, i);//data_for_test[i] == *(data+i)
     }
 }
