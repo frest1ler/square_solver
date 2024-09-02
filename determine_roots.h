4ffb5fa19aa2c9ef378b98a09a8a46303b936149
frest1ler <143139569+frest1ler@users.noreply.github.com>
2024-09-02 20:57:29 +0300

    Delete determine_roots.h

--- determine_roots.h
+++
@@ -1,7 +0,0 @@
-#ifndef DETERMINE_ROOTS_H
-#define DETERMINE_ROOTS_H
-
-void determine_roots(struct equation_coefficients *coefficients, struct equation_roots *roots);
-void square_solver(struct equation_coefficients *coefficients, struct equation_roots *roots);
-void linear_solver(struct equation_coefficients *coefficients, struct equation_roots *roots);
-#endif /*DETERMINE_ROOTS_H*/
