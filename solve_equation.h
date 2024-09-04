ba898385bdbac9de6ad83040a450a2e75f6e187e
frest1ler <143139569+frest1ler@users.noreply.github.com>
2024-09-04 11:54:13 +0300

    Delete solve_equation.h

--- solve_equation.h
+++
@@ -1,9 +0,0 @@
-#ifndef SOLVE_EQUATION_H
-#define SOLVE_EQUATION_H
-
-#include "square_solver.h"
-
-void solve_equation(const Equation_coefficients *coefficients, Equation_roots *roots);
-void solve_square(const Equation_coefficients *coefficients, Equation_roots *roots);
-void solve_linear(const Equation_coefficients *coefficients, Equation_roots *roots);
-#endif /*SOLVE_EQUATION_H*/
